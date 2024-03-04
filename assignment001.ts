d2a636f0c4f8d525ffb1919ab5c898086d704cc3
RuqaiyaGull <162179273+RuqaiyaGull@users.noreply.github.com>
2024-03-04 22:41:50 +0500

    Add files via upload

---
+++ assignment001.ts
@@ -0,0 +1,49 @@
+let number1:number=35
+let number2:number=40
+let number3:number=10
+let numberAdd:number=number1 + number2 + number3
+let numberSubtract:number=number2 -number1
+console.log(numberAdd )
+console.log(numberSubtract)
+
+
+let numberMultiply:number=numberAdd * numberSubtract
+console.log(numberMultiply)
+
+let numberDivide:number=numberMultiply
+
+
+//compersion Operators
+let comp1=numberDivide==100
+console.log(comp1)
+let comp2=numberDivide !=100
+console.log(comp2)
+let comp3=numberDivide >100
+console.log(comp3)
+let comp4=numberDivide <100
+console.log(comp4)
+let comp5=numberDivide>=100
+console.log(comp5)
+let comp6=numberDivide <=100
+console.log(comp6)
+
+
+//concatenation:
+console.log("concatenation")
+let name1:string="ruqaiya"
+let name2:string="gull"
+let Fullname:string=name1 + " " + name2
+console.log(Fullname)
+
+let num1:number=10
+let idName=name1 + num1
+console.log(idName)
+let num2:string="46"
+let idNumber=num1 + num2
+console.log(idNumber)
+let w = 50
+w += 5
+console.log(w)
+let s = 60
+s -= 30
+console.log(s)
